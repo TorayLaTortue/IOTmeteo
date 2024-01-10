0279c6cdbb712284d3dfdffdbafb0f8801ecaa0c
TorayLaTortue <damien.pasquer3636@gmail.com>
2024-01-10 11:10:39 +0100

    ecran

--- capteur.py
+++ capteur.py
@@ -1,5 +1,7 @@
 import requests
 import bme280
+import serial
+import time
 
 # Lecture des données du BME280
 (chip_id, chip_version) = bme280.readBME280ID()
@@ -13,15 +15,25 @@ payload = {
     'temperature': temperature,
     'humidite': humidity,
     'p_atmospherique': pressure,
-    'date': 'date_value',  # Remplacez par la valeur réelle de la date
-    'heure': 'heure_value'  # Remplacez par la valeur réelle de l'heure
-} # Je crois que sur la raspberry on peut recupérer l'heure ^
+}
 
-# lien du script de la bdd sur la raspberry
-url = 'http://votre_domaine.com/chemin/vers/votre_script.php'
+# Lien du script de la BDD sur la Raspberry
+url = 'votre_script.php'
 
 # Envoi de la requête POST avec le payload
 response = requests.post(url, data=payload)
 
 # Affichage de la réponse du serveur
 print(response.text)
+
+# Configuration de la connexion série avec l'écran Nextion
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)  # Assurez-vous de spécifier le bon port série
+
+# Attendez un court moment pour que l'écran Nextion soit prêt
+time.sleep(1)
+
+# Envoi de texte à l'écran Nextion
+ser.write(b't0.txt="Hello Nextion!"\xFF\xFF\xFF')
+
+# Fermeture de la connexion série
+ser.close()
