ecf1a64ea51f8a5be7194518f8a99930b8297d00
Clém <clemauger01@orange.fr>
2024-01-11 11:19:48 +0100

    graph ok

--- dashboard.php
+++ dashboard.php
@@ -37,9 +37,9 @@
     <div class="dashboard">
         <h2>Tableau de bord</h2>
         
-        <canvas id="lineCanvasTemp" aria-label="chart" role="img"></canvas>
-        <canvas id="barCanvasHumidité" aria-label="chart" role="img"></canvas>
-        <canvas id="barCanvasPression" aria-label="chart" role="img"></canvas>
+        <canvas id="lineCanvasTemp" aria-label="chart" role="img" width="200" height="200"></canvas>
+        <canvas id="lineCanvasHumidité" aria-label="chart" role="img" width="200" height="200"></canvas>
+        <canvas id="lineCanvasPression" aria-label="chart" role="img" width="200" height="200"></canvas>
         <!-- <script src ="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"></script> -->
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
--- script.js
+++ script.js
@@ -14,6 +14,8 @@ document.addEventListener("DOMContentLoaded", function() {
             }]
         },
         options: {
+            responsive: false,
+            
             elements: {
                 point: {
                     pointBorderColor: "#333"
@@ -37,48 +39,79 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // Graphique de Taux d'humidité
-    const barCanvasHumidité = document.getElementById('barCanvasHumidité');
-    const barChartHumidité = new Chart(barCanvasHumidité, {
-        type: "bar",
+    const lineCanvasHumidité = document.getElementById('lineCanvasHumidité');
+    const lineChartHumidité = new Chart(lineCanvasHumidité, {
+        type: "line",
         data: {
-            labels: ["1H", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "11H", "MIDI", "13H", "14H", "15H", "16H", "17H", "18H", "19H", "20H", "21H", "22H", "23H", "MINUIT"],
+            labels: ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
             datasets: [{
-                label: 'Taux d\'humidité',
+                label: 'Taux Humidité',
                 data: [20, 21, 19, 25, 23, 22, 20],
+                fill: false,
                 backgroundColor: 'red',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
+                tension: 0.1
             }]
         },
         options: {
+            responsive: false,
+            
+            elements: {
+                point: {
+                    pointBorderColor: "#333"
+                }
+            },
             scales: {
                 y: {
-                    beginAtZero: true
+                    ticks: {
+                        color: "#333"
+                    },
+                    suggestedMin: 0,
+                    suggestedMax: 40
+                },
+                x: {
+                    ticks: {
+                        color: "#333"
+                    }
                 }
             }
         }
     });
 
     // Graphique de Pression Atmosphérique
-    const barCanvasPression = document.getElementById('barCanvasPression');
-    const barChartPression = new Chart(barCanvasPression, {
-        type: "bar",
+    const lineCanvasPression = document.getElementById('lineCanvasPression');
+    const lineChartPression = new Chart(lineCanvasPression, {
+        type: "line",
         data: {
-            labels: ["1H", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "11H", "MIDI", "13H", "14H", "15H", "16H", "17H", "18H", "19H", "20H", "21H", "22H", "23H", "MINUIT"],
+            labels: ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
             datasets: [{
                 label: 'Pression Atmosphérique',
                 data: [20, 21, 19, 25, 23, 22, 20],
+                fill: false,
                 backgroundColor: 'red',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
+                tension: 0.1
             }]
         },
         options: {
+            responsive: false,
+            
+            elements: {
+                point: {
+                    pointBorderColor: "#333"
+                }
+            },
             scales: {
                 y: {
-                    beginAtZero: true
+                    ticks: {
+                        color: "#333"
+                    },
+                    suggestedMin: 0,
+                    suggestedMax: 40
+                },
+                x: {
+                    ticks: {
+                        color: "#333"
+                    }
                 }
             }
         }
-    });
-});
+    })});
